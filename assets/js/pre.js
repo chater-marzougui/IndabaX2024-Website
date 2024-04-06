5eca86f6662cedb44196c36746c7eca55909b7a5
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-04-06 21:04:01 +0100

    Initial commit

---
+++ assets/js/pre.js
@@ -0,0 +1,33 @@
+var loader = document.querySelector(".loader")
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+    loader.classList.add("disppear");
+    window.addEventListener("transitioned", removeCode);
+} 
+
+function removeCode() {
+    while (loader.hasChildNodes()) {
+      loader.removeChild(loader.firstChild);
+    }
+}
+
+
+//DARK - MODE
+const darkModeToggle = document.getElementById('darkModeToggle');
+        const body = document.body;
+        const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+        if (isDarkMode) {
+            body.classList.add('dark-mode');
+            darkModeToggle.checked = true;
+        }
+        darkModeToggle.addEventListener('change', () => {
+            if (darkModeToggle.checked) {
+                body.classList.add('dark-mode');
+                localStorage.setItem('darkMode', 'enabled');
+            } else {
+                body.classList.remove('dark-mode');
+                localStorage.setItem('darkMode', 'disabled');
+            }
+        });  
