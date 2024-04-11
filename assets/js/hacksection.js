4a839fbe5c95803341dc82e3654514f19d124b31
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-04-11 01:56:08 +0100

    final poster + hack sections

---
+++ assets/js/hacksection.js
@@ -0,0 +1,45 @@
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".trigger",
+      scrub: 0.5,
+      pin: true,
+      markers: true,
+      start: "-9% top",
+      end: "+=139%",
+    },
+  })
+  .to(".box", {
+    force3D: true,
+    duration: 1,
+    xPercent: 100,
+    ease: "power1.inOut",
+    stagger: { amount: 1 },
+  })
+  .to(".box", { ease: "power1.out", duration: 1, rotation: "45deg" }, 0)
+  .to(".box", { ease: "power1.in", duration: 1, rotation: "0deg" }, 1)
+  .fromTo(
+    ".zindi-img",
+    {
+      y: 20,
+    },
+    { y: -150 },
+    "-=1"
+  )
+  .to(
+    ".content-zindi--hide",
+    {
+      visibility: "hidden",
+      opacity: 0,
+      duration: 5,
+      ease: "power1.out",
+    },
+    "-=1"
+  )
+  .fromTo(
+    ".hackathon-zindi",
+    { y: -220, ease: "power1.out", duration: 1 },
+    { y: 0 },
+    "-=6"
+  )
+  .to(".content-hackathon--hide", { visibility: "visible", opacity: 1 }, "-=5");
