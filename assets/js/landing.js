06f64b803b77d3163d7f917c234549245d7ce22e
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-04-20 22:55:44 +0100

    landing changes + loader

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,52 +1,65 @@
 /** @format */
-const svg = document.getElementById("svg");
-const tl = gsap.timeline({
-  onComplete: () => {
-    document.body.style.overflowY = "visible";
-  },
-});
-const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
-/*
-tl.from(".loader-wrap-heading h1", {
-  delay: 0.5,
-  y: 200,
-  skewY: 20,
-}).to(".loader-wrap-heading h1", {
-  delay: 0.5,
-  y: -200,
-  skewY: 20,
-});*/
-tl.to(svg, {
-  duration: 0.8,
-  attr: { d: curve },
-  ease: "power2.easeIn",
-}).to(svg, {
-  duration: 0.8,
-  attr: { d: flat },
-  ease: "power2.easeOut",
-});
-tl.to(".loader-wrap", {
-  y: -1500,
-});
-tl.to(".loader-wrap", {
-  zIndex: -1,
-  display: "none",
-  duration:0.1,
-});
-tl.from(
-  "#hero, #Navbar",
-  {
-    opacity: 0,
-  },
-  "-=0.1"
-).to(
-  "#hero, #Navbar",
-  {
-    opacity: 1,
-  },
-  "+=1.5"
-);
+
+var loader = document.querySelector(".loader")
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+    
+        loader.classList.add("disppear");
+
+        // Add event listener for transition end after class is added
+        window.addEventListener("transitionend", removeCode);
+    setTimeout(function() {
+    const svg = document.getElementById("svg");
+    const tl = gsap.timeline({
+      onComplete: () => {
+        document.body.style.overflowY = "visible";
+      },
+    });
+    
+    //landing part
+    const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+    const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+    tl.to(svg, {
+      duration: 0.8,
+      attr: { d: curve },
+      ease: "power2.easeIn",
+    }).to(svg, {
+      duration: 0.8,
+      attr: { d: flat },
+      ease: "power2.easeOut",
+    });
+    tl.to(".loader-wrap", {
+      y: -1500,
+    });
+    tl.to(".loader-wrap", {
+      zIndex: -1,
+      display: "none",
+      duration:0.1,
+    });
+    tl.from(
+      "#hero, #Navbar",
+      {
+        opacity: 0,
+      },
+      "-=0.1"
+    ).to(
+      "#hero, #Navbar",
+      {
+        opacity: 1,
+      },
+      "+=1.5"
+    );
+  }, 1000);
+} 
+
+function removeCode() {
+    while (loader.hasChildNodes()) {
+      loader.removeChild(loader.firstChild);
+    }
+}
+
 
 
 
