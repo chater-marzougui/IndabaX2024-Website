5498886e43c1839f7b6d9f003a95d33be69a3a42
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-04-21 19:47:13 +0100

    Stay Tuned new countdown

--- assets/js/StayTuned.js
+++ assets/js/StayTuned.js
@@ -10,8 +10,15 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the countdown
+    document.getElementById("countdown").innerHTML = "<div style='font-size: 13vh;font-weight:600;'>" + days + "<br><p style='font-size: 6vh;'>Days</p></div><br>"
+    + "<div style='font-size: 9.5vh;'>" + hours + "<br><p style='font-size: 5vh;'>Hrs</p></div>"
+    + "<div style='font-size: 9.5vh;'> :" + minutes + "<br><p style='font-size: 5vh;'>Mn</p></div>"
+    + "<div style='font-size: 9.5vh;'> :" + seconds + "<br><p style='font-size: 5vh;'>Sec</p></div>";
+
+
+    /*
     document.getElementById("countdown").innerHTML = "<div>" + days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s </div>";
+    + minutes + "m " + seconds + "s </div>";*/
 
     if (distance < 0) {
         clearInterval(x);
