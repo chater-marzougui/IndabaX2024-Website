e40f8509f9affb161d1ff90e5cbf7f79d343628f
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-04-23 20:05:06 +0100

    Finallyyyyyyy

--- assets/js/hackathon.js
+++ assets/js/hackathon.js
@@ -1,24 +1,49 @@
 
 const descriptionPanel = document.querySelector('.description-panel');
+const widthp = window.innerWidth;
+console.log(widthp);
 gsap.registerPlugin(ScrollTrigger);
-
-gsap.utils.toArray(".hackathon-panel:not(.description-panel)").forEach((panel, i) => {
-    ScrollTrigger.create({
-        trigger: panel,
-        start: "top top",
-        endTrigger: "#poster-session",
-        end: "top bottom",
-        pin: true,
-        pinSpacing: false
+    if(widthp<=768){
+    gsap.utils.toArray(".hackathon-panel.red").forEach((panel, i) => {
+        ScrollTrigger.create({
+            trigger: panel,
+            start: "top top",
+            endTrigger: ".hackathon-panel.last-panel",
+            end: "top bottom",
+            pin: true,
+            pinSpacing: false,
+        });
     });
-});
-const pinDescriptionPanel = gsap.to(descriptionPanel, {
-    scrollTrigger: {
-        trigger: descriptionPanel,
-        start: "top top",
-        endTrigger: "#poster-session",
-        end: "top bottom",
-        pin: true,
-        pinSpacing: false,
-    },
-});
+    gsap.to(descriptionPanel, {
+        scrollTrigger: {
+            trigger: descriptionPanel,
+            start: "top top",
+            endTrigger: ".hackathon-panel.last-panel",
+            end: "top bottom",
+            pin: true,
+            pinSpacing: false,
+        },
+    });
+}
+if(widthp>768){
+    gsap.utils.toArray(".hackathon-panel:not(.description-panel)").forEach((panel, i) => {
+        ScrollTrigger.create({
+            trigger: panel,
+            start: "top top",
+            endTrigger: "#poster-session",
+            end: "top bottom",
+            pin: true,
+            pinSpacing: false
+        });
+    });
+    const pinDescriptionPanel = gsap.to(descriptionPanel, {
+        scrollTrigger: {
+            trigger: descriptionPanel,
+            start: "top top",
+            endTrigger: "#poster-session",
+            end: "top bottom",
+            pin: true,
+            pinSpacing: false,
+        },
+    });
+}
