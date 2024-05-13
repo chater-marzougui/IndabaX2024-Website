e4ee27f49152b2ad3f4d0859b19c6ec5c2e56947
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-05-13 11:35:59 +0100

    Closed Registration

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -61,7 +61,7 @@ function removeCode() {
 }
 
 
-var RcountDownDate = new Date("May 6, 2024 20:00:00").getTime();
+var RcountDownDate = new Date("May 6, 2150 20:00:00").getTime();
 function togglepopup() {
   var now = new Date().getTime();
   var distance = RcountDownDate - now;
