fc75349db816ef172f2d4afdcb98d476d23c4965
chater_marzougui <chater.mrezgui2002@gmail.com>
2024-05-15 17:19:31 +0100

    Program Scrolling Fix

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,30 +1,41 @@
 $(document).ready(function () {
-    (function ($) {
-      $.fn.timeline = function () {
-        var selectors = {
-          id: $(this),
-          item: $(this).find(".timeline-item"),
-          activeClass: "timeline-item--active",
+  (function ($) {
+    $.fn.timeline = function () {
+      var selectors = {
+        id: $(this),
+        item: $(this).find(".timeline-item"),
+        activeClass: "timeline-item--active",
+      };
+      selectors.item.eq(0).addClass(selectors.activeClass);
+      var itemLength = selectors.item.length;
+
+      var debounce = function (func, wait) {
+        var timeout;
+        return function () {
+          var context = this, args = arguments;
+          clearTimeout(timeout);
+          timeout = setTimeout(function () {
+            func.apply(context, args);
+          }, wait);
         };
-        selectors.item.eq(0).addClass(selectors.activeClass);
-        var itemLength = selectors.item.length;
-        $(window).scroll(function () {
-          var max, min;
-          var pos = $(this).scrollTop();
-          selectors.item.each(function (i) {
-            min = $(this).offset().top;
-            max = $(this).height() + $(this).offset().top;
-            var that = $(this);
-            // 
-            if (pos + $(window).height() / 3.5 >= min) {
-              selectors.item.removeClass(selectors.activeClass);
-              $(this).addClass(selectors.activeClass);
-            }
-          });
+      };
+
+      var onScroll = function () {
+        var pos = $(this).scrollTop();
+        selectors.item.each(function (i) {
+          var min = $(this).offset().top;
+          var max = $(this).height() + $(this).offset().top;
+          var that = $(this);
+          if (pos + $(window).height() / 3.5 >= min && pos < max) {
+            selectors.item.removeClass(selectors.activeClass);
+            $(this).addClass(selectors.activeClass);
+          }
         });
       };
-    })(jQuery);
-  
-    $("#program-section").timeline();
-  });
-  
+
+      $(window).scroll(debounce(onScroll, 100));
+    };
+  })(jQuery);
+
+  $("#program-section").timeline();
+});
